4ad01cb496089cfa2d6213bace50f2db3b3b0697
matorblal-byte <matorblal@gmail.com>
2026-03-12 22:31:50 -0400

    Update Archipelago.cpp

--- Archipelago.cpp
+++ Archipelago.cpp
@@ -801,13 +801,17 @@ bool parse_response(std::string msg, std::string &request) {
                     }
                     map_slotdata_callback_mapintint[key](out);
                 } else if (map_slotdata_callback_intlist.count(key)) {
-                    std::vector<int64_t> out;
-                        if (root[i]["slot_data"][key].isArray()) {
-                            for (Json::Value& val : root[i]["slot_data"][key]) {
-                                out.push_back(val.asInt64());
+                    const Json::Value& slot_val = root[i]["slot_data"][key];
+                    if (slot_val.isArray()) {
+                        std::vector<int64_t> out;
+                        out.reserve(slot_val.size());
+                
+                        for (const auto& element : slot_val) {
+                            if (!element.isNull()) {
+                                out.push_back(element.asInt64());
                             }
-                            map_slotdata_callback_intlist[key](out);
                         }
+                        map_slotdata_callback_intlist[key](out);
                 } else {
                     printf("AP: Warning: Unmapped slot data with key \"%s\"!\n", key.c_str());
                 }
